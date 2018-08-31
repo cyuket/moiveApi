136438d94f5a30af8d6f6620540cac17d0f77f43
syziddy65 <cyriluket12@gmail.com>
2018-08-31 05:42:20 +0100

    The details page is working

--- js/topRatedTv.js
+++ js/topRatedTv.js
@@ -4,6 +4,8 @@ news.controller('labController', [
     function ($scope, $http, $rootScope) {
     	$scope.model={};
     	$scope.movies = [];
+       $scope.showDetails=showDetails;
+       $scope.showMe = false;
 
 		var url = 'https://api.themoviedb.org/3/tv/top_rated?api_key=ea12c869f716552bef3493abf72c3617&language=en-US&page=1'
 		//var url = 'https://api.themoviedb.org/3/movie/popular?api_key=ea12c869f716552bef3493abf72c3617&language=en-US&page=1';
@@ -16,27 +18,26 @@ news.controller('labController', [
           		console.log(error);
           		$scope.show = false;
           });
-    	function getId(id) {
-    		// body...
-    		$rootScope.idd = id;
+    	function showDetails(id) {
+        // body...
+        
+        var url = 'https://api.themoviedb.org/3/tv/'+id+'?api_key=ea12c869f716552bef3493abf72c3617&language=en-US&page=1'
+        $http.get(url)
+        .then(function (response) {
+        $scope.detail = response.data;
+        console.log(response.data);
+        $scope.showMe = !$scope.showMe;
+        }, function (response) {
+            $scope.model.detail = { error: true, message: 'Error: ' + response.data.message };
+          });
 
-    	}
+
+      }
+      $scope.myFunc = function() {
+        $scope.showMe = false;
+    }
 
          
-    	
-}]);
-//var api= angular.module('myApp',[]);
-news.controller('detailController',['$scope','$http',function ($scope,$http) {
-
-    var url = 'https://api.themoviedb.org/3/movie/157336?api_key=ea12c869f716552bef3493abf72c3617&language=en-US&page=1&append_to_response=videos,images'
-    $http.get(url)
-    .then(function (response) {
-        $scope.detail = response.data;
-       	console.log(response.data);
-       	}, function (response) {
-            $scope.model.detail = { error: true, message: 'Error: ' + response.data.message };
-    });
-          
-
+      
 }]);
 
